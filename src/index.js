540d287b5d3f4de5d1d7f0877193941a5636312c
Daniel Martinez <roanhawkeye@gmail.com>
2022-03-24 17:45:09 -0600

    Refactor loading latitude from constructor to componentDidMount method

--- src/index.js
+++ src/index.js
@@ -9,14 +9,12 @@ class App extends React.Component {
       lat: null,
       errorMessage: "",
     };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (error) => {
-        this.setState({ errorMessage: error.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (error) => this.setState({ errorMessage: error.message })
     );
   }
 
