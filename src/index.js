edd035e9dac488de02fab2b99b79b5b31e03d430
Daniel Martinez <roanhawkeye@gmail.com>
2022-03-24 09:58:59 -0600

    Refactor App into a class componenent and set state for lalitude value

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (error) => console.log(error)
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return <div>Latitude: </div>;
-};
+    this.state = {
+      lat: null,
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (error) => console.log(error)
+    );
+  }
+
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
