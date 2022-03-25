27124581276915ab2e4ab8e1792be3e5cb6f436d
Daniel Martinez <roanhawkeye@gmail.com>
2022-03-25 09:28:47 -0600

    Add season display to app component and season identification logic

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>Season Displayed!!</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text =
+    season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
+
+  return (
+    <h1>
+      <div>{text}</div>
+    </h1>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import SeasonDisplay from "./SeasonDisplay";
+
 class App extends React.Component {
   state = {
     lat: null,
@@ -20,7 +22,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat} </div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading...!</div>;
